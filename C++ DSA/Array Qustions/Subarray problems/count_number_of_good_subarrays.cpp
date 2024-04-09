f5908812e2830305d5aba3eb280389283e3a8825
AreYouKhush <khushal9587@gmail.com>
2024-04-09 23:47:35 +0530

    time required to buy tickets

---
+++ C++ DSA/Array Qustions/Subarray problems/count_number_of_good_subarrays.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int numberOfSubarrays(vector<int>& nums, int k) {
+    
+}
+
+int main(){
+    vector<int> inp = {2,4,1,2,3,4,5,2,4,6};
+    int k = 2;
+    cout<<numberOfSubarrays(inp, k);
+}
---
+++ C++ DSA/Array Qustions/time_needed_to_buy_tickets.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int timeRequiredToBuy(vector<int>& tickets, int k) {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int cielNum = tickets[k];
+    int time = 0;
+    for(int i = 0; i < tickets.size(); i++){
+        if(i <= k){
+            time += tickets[i] >= cielNum ? cielNum : tickets[i];
+        }else {
+            time += tickets[i] >= cielNum - 1 ? cielNum - 1 : tickets[i];
+        }
+    }
+    return time;
+}
+
+int main(){
+    vector<int> inp = {2,3,4,56,3,45,56,6,34,45,2};
+    cout<<timeRequiredToBuy(inp ,4);
+}
