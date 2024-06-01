10c97e4e4f2c8a1fc3013dda8f9abbc85c6f8b58
AreYouKhush <khushal9587@gmail.com>
2024-06-01 23:43:55 +0530

    update: recursion

---
+++ C++ DSA/Recursion Backtracking/tiles_posibilities.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int sum = 0;
+    string str = "";
+    unordered_map<char, int> mpp;
+
+    void helper(string& tiles, int maxSize) {
+        if (!str.empty()) {
+            sum++;
+        }
+        for (auto& [key, count] : mpp) {
+            if (count > 0) {
+                str += key;
+                mpp[key]--;
+                helper(tiles, maxSize);
+                mpp[key]++;
+                str.pop_back();
+            }
+        }
+    }
+
+    int numTilePossibilities(string tiles) {
+        ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        for (char c : tiles) {
+            mpp[c]++;
+        }
+        helper(tiles, tiles.size());
+        return sum;
+    }
+};
